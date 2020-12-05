f2291ad0ad520da33cbc4192dcc37c2dc68abb28
Carson Brown <carsonbrown47@gmail.com>
2020-12-05 17:43:42 -0500

    refactored to make custom commands

--- bot.py
+++ bot.py
@@ -1,10 +1,18 @@
 import discord
 import os
-from MyClient import MyClient
+from discord.ext import commands
 
 intents = discord.Intents.all()
 f = open("C://Users//carso//Desktop//PA_bot_token.txt", 'r')
 token = f.read()
 
-client = MyClient(intents=intents, command_prefix='.')
-client.run(token)#runs the bot using the token that was read from the txt file
+# client = MyClient(intents=intents)
+# client.run(token)#runs the bot using the token that was read from the txt file
+
+bot = commands.Bot(intents=intents, command_prefix='$$')
+
+for fn in os.listdir('./cogs'):
+    if fn.endswith('.py'):
+        bot.load_extension(f'cogs.{fn[:-3]}')
+
+bot.run(token)
