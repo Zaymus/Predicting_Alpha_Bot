b59c50ca50780ac4e6a5474a02c3ca0c3bd919e7
Carson Brown <carsonbrown47@gmail.com>
2020-12-05 15:04:18 -0500

    added fix markdown for membership validation of message

--- welcome.py
+++ welcome.py
@@ -4,7 +4,7 @@ __***Welcome to the Predicting Alpha Virtual Trading Floor (VTF).***__
 
 The VTF represents a world of potential colleagues and collaborators. Ask questions, share insight, receive feedback on your ideas and network with the best traders from around the world.
 
-```VALIDATE YOUR MEMBERSHIP:  Send @SeanPA a message with your full name. He will assign you a community rank, unlocking all the chatrooms for you.```
+```fix\nVALIDATE YOUR MEMBERSHIP:  Send @SeanPA a message with your full name. He will assign you a community rank, unlocking all the chatrooms for you.```
 ```Community Rules & Guidelines
 1) Nicknames: Pick a professional name/alias that you want to be called by.
 2) Use the different channels for their intended purposes.
