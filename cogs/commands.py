5c9d2890eb48e2f9356604b77ed5c696dc3eba14
Carson Brown <carsonbrown47@gmail.com>
2020-12-05 17:44:33 -0500

    created basic commands

---
+++ cogs/commands.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+import os
+
+class Commands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot;
+
+    @commands.command()
+    async def ping(self, ctx):
+        ping = round(self.bot.latency * 1000)#gets ping of the user from the bot and converts it to ms instead of seconds
+
+        if(ping <= 80):
+            await ctx.send(f':satellite:```diff\n PONG!\n+ {ping}ms```:satellite_orbital:')
+        elif(ping <= 150):
+            await ctx.send(f':satellite:```fix\nPONG!\n+ {ping}ms```:satellite_orbital:')
+        else:
+            await ctx.send(f':satellite:```diff\nPONG!\n- {ping}ms```:satellite_orbital:')
+
+    @commands.command()
+    async def ranks(self, ctx):
+        await ctx.message.author.send('WIP')
+        await ctx.send('I have sent you a dm with the ranks are explained. if you need any other clarification, feel free to ask a PA staff member.')
+
+
+def setup(bot):
+    bot.add_cog(Commands(bot))
