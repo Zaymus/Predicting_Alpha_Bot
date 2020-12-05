02a4f794cd3259ef693e737e4dd8b4175c47ad4d
Carson Brown <carsonbrown47@gmail.com>
2020-12-05 14:48:18 -0500

    removed token and added file path to retrieve discord token

--- bot.py
+++ bot.py
@@ -4,7 +4,8 @@ from MyClient import MyClient
 
 intents = discord.Intents.all()
 
-token = "Nzg0NjA5MDg3Mzk2MzgwNjcz.X8ryJw.aW3S_bCh8JNCCOpv7Ius0i9HeZ8"
+f = open("C://Users//carso//Desktop//PA_bot_token.txt", 'r')
+token = f.read()
 
 client = MyClient(intents=intents, command_prefix='.')
 client.run(token)#runs the bot using the token that was read from the txt file
