2ae81986e755bf7fe89d2f10f22e99e50d53c007
Carson Brown <carsonbrown47@gmail.com>
2020-12-05 15:03:58 -0500

    added on connect and on disconnect events

--- MyClient.py
+++ MyClient.py
@@ -3,9 +3,15 @@ from welcome import welcome
 
 class MyClient(discord.Client):
 
+    async def on_connect(self):
+        print("Connection to Discord established!")
+
+    async def on_disconnect(self):
+        print("Connection to Discord terminated.")
+
     async def on_ready(self):
-        print(f'{self.user} is now active')
+        print(f'{self.user} is now active and ready for use!')
+
 
     async def on_member_join(self, member):
-        for message in welcome_message:
-            await member.send(message)
+        await member.send(welcome.message)
