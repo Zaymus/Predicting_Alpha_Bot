52d2608a04f755dade1c27a1b8de51ea8553d4fe
Carson Brown <carsonbrown47@gmail.com>
2020-12-05 18:27:59 -0500

    formatting

--- bot.py
+++ bot.py
@@ -2,14 +2,9 @@ import discord
 import os
 from discord.ext import commands
 
-intents = discord.Intents.all()
+bot = commands.Bot(intents=discord.Intents.all(), command_prefix='$$')
 f = open("C://Users//carso//Desktop//PA_bot_token.txt", 'r')
 token = f.read()
-
-# client = MyClient(intents=intents)
-# client.run(token)#runs the bot using the token that was read from the txt file
-
-bot = commands.Bot(intents=intents, command_prefix='$$')
 
 for fn in os.listdir('./cogs'):
     if fn.endswith('.py'):
